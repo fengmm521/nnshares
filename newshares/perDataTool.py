d3b93686de6a99d29433ca6c0043bfa3416d5b14
woodcol <mage@woodcol.com>
2017-11-13 02:31:22 +0800

    新加网易数据下载出错时从tushare下载数据

--- newshares/perDataTool.py
+++ newshares/perDataTool.py
@@ -13,11 +13,11 @@ txtCoding = 'utf-8'
 class MyException(Exception):  
         pass 
 
-def getFront60Dat(codeid,datcount = 60):
+def getFront60Dat(codeid,datcount = 60,pyear = 2017):
 
     outlist = []
 
-    tmpth = dbDir + os.sep + '%s.csv'%(codeid)
+    tmpth = dbDir + os.sep + str(pyear) + os.sep + '%s.csv'%(codeid)
     if not os.path.exists(tmpth):
         print '股票%s不存在，查看是否下地了这支股票的数据'%(tmpth)
         return None
@@ -42,18 +42,20 @@ def getFront60Dat(codeid,datcount = 60):
         if outlist[k][2][0] != 'N':
             k += 1
         else:
-            print 'tid:%s,new k is:%d'%(codeid,k)
             break
-    outlist = outlist[k:k+datcount]
+    if k != 0:
+        print 'tid:%s,new k is:%d'%(codeid,k)
 
+    if k + datcount < len(outlist):
+        outlist = outlist[k:k+datcount]
+    elif k != 0:
+        outlist = outlist[k:]
 
     return outlist
 
 
 
-
-
-def main():
+def getPerDataWithYear(pyear):
     f = open('sharelist.csv','r')    #打开文件
     lines = f.readlines()            #按行读取数据，保存一个数组中
     f.close()                        #关闭打开的文件
@@ -74,23 +76,48 @@ def main():
         tid = tmps[0]                #获取这一行的股票编号
         startDate = int(tmps[15])    #获取这支股票的发行日期,并把原来是字符串的日期文本转换成整数
 
-        if startDate > 20160000 and startDate < 20170000:   #获取2016的所有新发行股票ID
+        minyear = pyear * 10000
+        maxyear = (pyear + 1) * 10000
+
+        if startDate > minyear and startDate < maxyear:   #获取2016的所有新发行股票ID
             ids.append([tid,startDate])
 
     ids.sort(key=lambda x:x[1], reverse=False)
 
-    print ids
 
+    print len(ids)
 
     datdic = {}
 
+    erroids = []
+
     for i in ids:
         tid = i[0]
-        print i
         fdat = getFront60Dat(tid,60)
         if fdat:
             datdic[tid] = fdat
+        else:
+            erroids.append(tid)
+
+    return datdic,erroids
+
+def downDataWithTuShare(tid):
+    import tushare as ts
+    tmpth = dbDir + os.sep + str(pyear) + os.sep + '%s.csv'%(codeid)
+    
+    dat = ts.get_hist_data('300710') #一次性获取全部日k线数据
+    dat.to_csv(tmpth)
+
+def main():
+    datdic,erroids = getPerDataWithYear(2017)
+    print 'heave data count is:',len(datdic.keys())
+    print 'not data ids count is:',len(erroids)
+
+def test():
+    urlstr = "http://quotes.money.163.com/service/chddata.html?code=0"+ '300710' +"&start="+ '20160101' +"&end=" + '20171111'
+    print urlstr
 
 
 if __name__ == '__main__':  
     main()
+    # test()
--- newshares/sharesDataDown.py
+++ newshares/sharesDataDown.py
@@ -4,6 +4,7 @@ import os,sys
 import time
 import urllib2
 import socket  
+import tushare as ts
 #将所有Excel文件转为xml文件
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
@@ -16,7 +17,35 @@ if not os.path.exists(dbDir):
 class MyException(Exception):  
         pass 
 
-def downDBFunc(codeid,startdate,enddate):
+def isTodayDown(fpth):
+    ptime = int(os.path.getctime(fpth))
+    loctim = time.localtime(int(ptime))
+    numdattmp = int(loctim.tm_year)*10000 + int(loctim.tm_mon)*100 + int(loctim.tm_mday)
+    loctim = time.localtime()
+    strdate = int(loctim.tm_year)*10000 + int(loctim.tm_mon)*100 + int(loctim.tm_mday)
+
+    if numdattmp == strdate:
+        return True
+    else:
+        return False
+
+
+def isDownFileOK(datastr):
+    tmps = datastr.replace('\r','')
+    lines = tmps.split('\n')
+    if len(lines[-1]) < 5: 
+        lines = lines[:-1]
+    if len(lines) > 1:
+        return True
+    else:
+        return False
+
+
+def downDBFunc(codeid,startdate,enddate,savedir):
+    savepth = savedir + os.sep + codeid+'.csv'
+    if os.path.exists(savepth) and isTodayDown(savepth):
+        print '%s is heave download'%(codeid)
+        return True
     try:  
         urlstr = ""
         print codeid
@@ -30,12 +59,17 @@ def downDBFunc(codeid,startdate,enddate):
         # restr.add_header('Range', 'bytes=0-20')
         resque = urllib2.urlopen(req) 
         datatmp = resque.read()
-        savepth = dbDir + os.sep + codeid+'.csv'
-        f = open(dbDir + os.sep + codeid+'.csv','w')
-        f.write(datatmp)
-        f.close()
-        print '下载%s完成,保存在:%s'%(codeid,savepth)
-        return True
+
+        if isDownFileOK(datatmp):
+            if os.path.exists(savepth):
+                os.remove(savepth)
+            f = open(savepth,'w')
+            f.write(datatmp)
+            f.close()
+            print '下载%s完成,保存在:%s'%(codeid,savepth)
+            return True
+        else:
+            return False
     except urllib2.URLError, e:  
         if isinstance(e.reason, socket.timeout):  
             raise MyException("There was an error: %r" % e)  
@@ -44,13 +78,18 @@ def downDBFunc(codeid,startdate,enddate):
             raise
     print '下载%s数据失败...'%(codeid)
     return False
-def main():
+
+def downDataWithTuShare(tid,pyear):
+    tmpth = dbDir + os.sep + str(pyear) + os.sep + '%s.csv'%(codeid)
+    dat = ts.get_hist_data(tid) #一次性获取全部日k线数据
+    dat.to_csv(tmpth)
+
+def downYearShareDat(pyear):
     f = open('sharelist.csv','r')    #打开文件
     lines = f.readlines()            #按行读取数据，保存一个数组中
     f.close()                        #关闭打开的文件
 
     print len(lines)                 #输出一共有多少行
-
 
     ids = []                         #定义一个空数组，我们要获取的数据之后会保存在这个数组中
 
@@ -65,22 +104,58 @@ def main():
         tid = tmps[0]                #获取这一行的股票编号
         startDate = int(tmps[15])    #获取这支股票的发行日期,并把原来是字符串的日期文本转换成整数
 
-        if startDate > 20160000 and startDate < 20170000:   #获取2016的所有新发行股票ID
+        minyear = pyear * 10000
+        maxyear = (pyear + 1) * 10000
+        if startDate > minyear and startDate < maxyear:   #获取2016的所有新发行股票ID
             ids.append([tid,startDate])
 
     ids.sort(key=lambda x:x[1], reverse=False)
 
     print ids
 
+    yearpth = dbDir + os.sep + str(pyear)
+    if not os.path.exists(yearpth):
+        os.mkdir(yearpth)
 
     downErroIDs = []
+
+    startdownday = str(pyear) + '0101'
+    enddownday = str(pyear + 1) + '0601' 
 
     for i in ids:
         tid = i[0]
         print i
-        isDown = downDBFunc(tid, '20160101', '20170401')
+        time.sleep(0.05)
+        isDown = downDBFunc(tid, startdownday, enddownday,yearpth)
         if not isDown:
             downErroIDs.append(i)
 
+    if downErroIDs:
+        print downErroIDs
+        print 'redown erro ids with TuShare'
+        dcount = len(downErroIDs)
+        for i in downErroIDs:
+            print 'redwon %s,ddown count is:%d'%(i,dcount)
+            downDataWithTuShare(i, pyear)
+            dcount -= 1
+            print '%s download is ok and sleep 5s...'%(i)
+            time.sleep(1)
+            print '4'
+            time.sleep(1)
+            print '3'
+            time.sleep(1)
+            print '2'
+            time.sleep(1)
+            print '1'
+            time.sleep(1)
+    return ids
+
+def main():
+    downYearShareDat(2017)
+
+def test():
+    print isTodayDown('sharedata/2017/002774.csv')
+
 if __name__ == '__main__':  
     main()
+    # test()
